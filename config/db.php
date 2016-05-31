836a050a2d1f1306cf9c768087040938a456e996
Mrad Omar <omar.mrad@globant.com>
2016-05-31 02:35:55 -0300

    Cambiando el nombre de la base de datos

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=historia',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
