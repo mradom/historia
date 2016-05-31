2293b5f2a744780ead26bab7c098d02229e075a1
Mrad Omar <omar.mrad@globant.com>
2016-05-31 04:32:12 -0300

    Se agrega la configuracion de UrlManager, para las url amigables

--- config/web.php
+++ config/web.php
@@ -46,6 +46,18 @@ $config = [
             ],
         ],
         */
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
+            'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => [
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ],
+        ],
     ],
     'params' => $params,
 ];
