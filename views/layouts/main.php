3269d333215281dd73c770f1eb2ddbc8a4dff0d6
Mrad Omar <omar.mrad@globant.com>
2016-05-31 04:33:05 -0300

    Se cambia el texto a español

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -37,6 +37,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Pacientes', 'url' => ['/paciente/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
